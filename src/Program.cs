b2b57c13d7d0dfb0c946b7fbc2da86c97c2065c9
PhantomNeon <guptaishaan06@gmail.com>
2022-02-02 00:11:22 +0530

    Some random refining of code

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.5")]
 namespace Synkrino;
 public class Program
 {
+    public static readonly string HelpText = "Available Commands:\n     compare:     Does comparison.\n     exit:     tExits the Synkrino environment.";
+
     public static void Main()
     {
         #region Uncomment on release
-        Console.WriteLine($"Synkrino Ver.{Assembly.GetExecutingAssembly().GetName().Version}\nUse the 'help' or 'credits' command for more information, use the 'exit' command to exit the environment.");
+        Console.WriteLine($"Synkrino Ver. {Assembly.GetExecutingAssembly().GetName().Version}\nUse the 'help' or 'credits' command for more information, use the 'exit' command to exit the environment.");
         
         while (true)
         {
             try
             {
-                Console.Write(">> ");
+                Console.Write(">>> ");
                 string[] arguements = Console.ReadLine()!.Split(' ', 100);
 
                 switch (arguements[0])
@@ -22,17 +24,37 @@ public class Program
                         Environment.Exit(0);
                         break;
 
-                    case "help":
-                        Console.WriteLine("Command still in development.");
+                    case "help" when arguements.Length == 1:
+                        Console.WriteLine(HelpText);
                         break;
 
-                    case "compare" when (arguements.Length == 4 && arguements[2] == "to") :
+                    case "help" when arguements.Length == 2:
+                    {
+                        switch (arguements[1])
+                        {
+                            case "compare":
+                                Console.WriteLine("Raid Shadow Legends is a-");
+                                break;
+                            
+                            default:
+                                EncounterInvalidCommand();
+                                break;
+                        }
+                        break;
+                    }
+
+                    case "compare" when arguements.Length == 4 && arguements[2] == "to" :
                     {    
-                        string? file1 = GetFile(arguements[1]);
-                        string? file2 = GetFile(arguements[3]);
+                        string? file1 = GetFileFromPath(arguements[1]);
+                        string? file2 = GetFileFromPath(arguements[3]);
 
                         Compare compare = new(file1, file2);
-                        ReturnAnalysis(compare.TreeBeingCompared);
+                        ReturnAnalysis(compare.TreeBeingCompared, compare.TreeBeingComparedTo);
+                        break;
+                    }
+
+                    case "compare" when arguements.Length == 4 && arguements[2] == "from" :
+                    {    
                         break;
                     }
 
@@ -40,7 +62,7 @@ public class Program
                         break;
 
                     default:
-                        Console.WriteLine("Invalid syntax or command!");
+                        EncounterInvalidCommand();
                         break;
                 }
             }
@@ -53,8 +75,13 @@ public class Program
         }
         #endregion
     }
-
-    public static string? GetFile(string path)
+    private static void EncounterInvalidCommand()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid syntax or command!");
+        Console.ResetColor();
+    }
+    private static string? GetFileFromPath(string path)
     {
         if(path == null)
             return "";
@@ -68,9 +95,9 @@ public class Program
         return finalData;
         
     }
-    public static void ReturnAnalysis(DynamicSyntaxTree treeBeingCompared)
+    private static void ReturnAnalysis(DynamicSyntaxTree treeBeingCompared, DynamicSyntaxTree treeBeingComparedTo)
     {
-        if(!treeBeingCompared.HasDynamicChildren)
+        if(!treeBeingCompared.HasDynamicChildren && !treeBeingComparedTo.HasDynamicChildren)
         {
             Console.WriteLine("The code has no dissimilarities.");
             return;
@@ -85,8 +112,18 @@ public class Program
                 PrintLine(child);
             }
         }
+
+        if(treeBeingComparedTo.HasDynamicChildren)
+        {
+            Console.WriteLine("The following lines are missing from the reference file:");
+
+            foreach (DynamicSyntaxTree child in treeBeingCompared.Children)
+            {
+                PrintLine(child);
+            }
+        }
     }
-    public static void PrintLine(DynamicSyntaxTree tree)
+    private static void PrintLine(DynamicSyntaxTree tree)
     {
         if (!tree.HasDynamicChildren)
         {
